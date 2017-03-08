db11e7df8b88eab011bb0feb6ee2a855d196428e
Aaron Colaco <aaron_colaco@persistent.com>
2017-03-08 17:08:27 +0530

    Renamed chaincode files

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -1,39 +1,23 @@
-/*
-Copyright IBM Corp. 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
-
 import (
-  "errors"
-  "fmt"
-  "strconv"
-
-  "github.com/hyperledger/fabric/core/chaincode/shim"
+    "encoding/json"
+    "fmt"
+    "errors"
+    "strconv"
+    "github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
+type SimpleChaincode struct{
 }
 
+type Owner struct{
+  Name  string  `json:"Name"`
+  Address string  `json:"Address"`
+  Number int64  `json:"Number"`
+}
+
+// Init : Adds initial block to chaincode on blockchain network
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
   var A, B string    // Entities
   var Aval, Bval int // Asset holdings
@@ -70,114 +54,80 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
   return nil, nil
 }
 
-// Transaction makes payment of X units from A to B
+
+// Invoke : Adds a new block to the blockchain network
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-  if function == "delete" {
-    // Deletes an entity from its state
-    return t.delete(stub, args)
+  if function == "init" {
+    // Initial block - Puts 'abc' and '99'
+    return t.Init(stub, "init", args)
+  } else if function == "createContact" {
+    // Pushes property details to Blockchain network
+    return t.createContact(stub, args)
   }
 
-  var A, B string    // Entities
-  var Aval, Bval int // Asset holdings
-  var X int          // Transaction value
-  var err error
+  fmt.Println("invoke did not find func: " + function) //error
+  return nil, errors.New("Received unknown function invocation")
+}
+
+func (t *SimpleChaincode) createContact(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
   if len(args) != 3 {
-    return nil, errors.New("Incorrect number of arguments. Expecting 3")
+    return nil, errors.New("Incorrect number of arguments. Expected 3 arguments")
   }
 
-  A = args[0]
-  B = args[1]
-
-  // Get the state from the ledger
-  // TODO: will be nice to have a GetAllState call to ledger
-  Avalbytes, err := stub.GetState(A)
-  if err != nil {
-    return nil, errors.New("Failed to get state")
-  }
-  if Avalbytes == nil {
-    return nil, errors.New("Entity not found")
-  }
-  Aval, _ = strconv.Atoi(string(Avalbytes))
-
-  Bvalbytes, err := stub.GetState(B)
-  if err != nil {
-    return nil, errors.New("Failed to get state")
-  }
-  if Bvalbytes == nil {
-    return nil, errors.New("Entity not found")
-  }
-  Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-  // Perform the execution
-  X, err = strconv.Atoi(args[2])
-  if err != nil {
-    return nil, errors.New("Invalid transaction amount, expecting a integer value")
-  }
-  Aval = Aval - X
-  Bval = Bval + X
-  fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-  // Write the state back to the ledger
-  err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-  if err != nil {
-    return nil, err
-  }
-
-  err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-  if err != nil {
-    return nil, err
-  }
-
-  return nil, nil
-}
-
-// Deletes an entity from state
-func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-  if len(args) != 1 {
-    return nil, errors.New("Incorrect number of arguments. Expecting 1")
-  }
-
-  A := args[0]
-
-  // Delete the key from the state in ledger
-  err := stub.DelState(A)
-  if err != nil {
-    return nil, errors.New("Failed to delete state")
-  }
-
-  return nil, nil
-}
-
-// Query callback representing the query of a chaincode
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-  if function != "query" {
-    return nil, errors.New("Invalid query function name. Expecting \"query\"")
-  }
-  var A string // Entities
   var err error
+  // Reference to struct
+  var owner Owner
 
-  if len(args) != 1 {
-    return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
-  }
+  //Set key
+  var ownerName = args[0]
+  owner.Name = ownerName
 
-  A = args[0]
+  var addressValue = args[1]
+  owner.Address = addressValue
 
-  // Get the state from the ledger
-  Avalbytes, err := stub.GetState(A)
+  var contactValue int64
+  contactValue, _ = strconv.ParseInt(args[2], 10, 64)
+  owner.Number = contactValue
+
+  // Convert to bytes and store in blockchain
+
+  bytes,_ := json.Marshal(owner)
+  err = stub.PutState(ownerName, bytes)
+
   if err != nil {
-    jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-    return nil, errors.New(jsonResp)
+    return nil, errors.New("Putstate failed")
   }
 
-  if Avalbytes == nil {
-    jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-    return nil, errors.New(jsonResp)
+  return nil,nil
+}
+
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+  fmt.Println("query is running " + function)
+
+  // Handle different functions
+  if function == "readContact" {
+    return t.readContact(stub, args)
   }
 
-  jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-  fmt.Printf("Query Response:%s\n", jsonResp)
-  return Avalbytes, nil
+  return nil, errors.New("Received unknown function query: " + function)
+}
+
+
+func (t *SimpleChaincode) readContact(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+  if len(args) != 1 {
+    return nil, errors.New("Invalid numbers of arguments. Expected ")
+  }
+
+  var ownerName = args[0]
+
+  var owner Owner
+
+  valAsBytes,_ := stub.GetState(ownerName)
+
+  json.Unmarshal(valAsBytes,&owner)
+
+  return valAsBytes,nil
 }
 
 func main() {
@@ -186,3 +136,6 @@ func main() {
     fmt.Printf("Error starting Simple chaincode: %s", err)
   }
 }
+
+
+
